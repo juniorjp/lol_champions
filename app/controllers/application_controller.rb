8bdc5466cd7e4fda92055018ae9de96ac32b7cea
Reginaldo Junior <juniorjp1989@gmail.com>
2016-10-01 20:07:30 -0300

    add locale dectection

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,21 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_action :set_locale
+
+  def set_locale
+    if params.has_key?(:locale)
+      @api_locale = params[:locale]
+    elsif session['locale']
+      @api_locale = session['locale']
+    else
+      @api_locale = extract_locale_from_accept_language_header
+    end
+  end
+
+  private
+
+  def extract_locale_from_accept_language_header
+    request.env['HTTP_ACCEPT_LANGUAGE'] ? request.env['HTTP_ACCEPT_LANGUAGE'].scan(/^[a-z]{2}/).first : 'pt_BR'
+  end
+
 end

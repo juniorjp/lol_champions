276e1d8701593f4663d2dbc0e36556c37e29fb32
Reginaldo Junior <juniorjp1989@gmail.com>
2016-10-01 20:11:00 -0300

    add list champions endpoint

--- app/controllers/champions_controller.rb
+++ app/controllers/champions_controller.rb
@@ -1,2 +1,14 @@
 class ChampionsController < ApplicationController
+
+  def index
+    query = {
+      locale: @api_locale,
+      champData: 'all',
+      api_key: ENV['lol_api_key']
+    }
+
+    response = HTTParty.get("#{ENV['lol_api_base_url']}#{ENV['lol_api_list_champion_endpoint']}", query: query)
+    render json: response.body, status: response.code
+  end
+
 end

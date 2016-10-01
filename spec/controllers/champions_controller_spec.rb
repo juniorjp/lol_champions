4ac525c4fe32119a3ea426d7edae68d1da4f8c04
Reginaldo Junior <juniorjp1989@gmail.com>
2016-10-01 19:32:00 -0300

    add champions controller with failing tests

---
+++ spec/controllers/champions_controller_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+
+RSpec.describe ChampionsController, type: :controller do
+
+  describe "GET #index" do
+    it "responds successfully with an HTTP 200 status code" do
+      get :index
+      expect(response).to be_success
+      expect(response).to have_http_status(200)
+    end
+
+    it "list all League of Legends Champions" do
+      #nowadays there are 132 registered champions
+      get :index
+      expect(response).to be_success
+      expect(response.body['data']).to be >= 132
+    end
+
+  end
+
+end

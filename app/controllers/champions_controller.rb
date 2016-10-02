a13fe78a1624c73f65ceb26b19fab29dea86a861
Reginaldo Junior <juniorjp1989@gmail.com>
2016-10-01 23:23:49 -0300

    small locale fix

--- app/controllers/champions_controller.rb
+++ app/controllers/champions_controller.rb
@@ -2,7 +2,7 @@ class ChampionsController < ApplicationController
 
   def index
     query = {
-      locale: @api_locale,
+      locale: @api_locale.start_with?('pt') ? 'pt_BR': 'en_US',
       champData: 'all',
       api_key: ENV['lol_api_key']
     }

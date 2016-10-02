63b7550a2ae9e5f3258755d40578d3f8f777740f
Reginaldo Junior <juniorjp1989@gmail.com>
2016-10-02 01:36:26 -0300

    implement items index action to pass the tests

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,12 @@
 class ItemsController < ApplicationController
 
   def index
+    query = {
+        locale: @api_locale.start_with?('pt') ? 'pt_BR': 'en_US',
+        api_key: ENV['lol_api_key']
+    }
+
+    response = HTTParty.get("#{ENV['lol_api_base_url']}#{ENV['lol_api_list_items_endpoint']}", query: query)
+    render json: response.body, status: response.code
   end
 end

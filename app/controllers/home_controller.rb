825b166dbbeaf0663124ea1d6f30b59a8fea3d7b
Reginaldo Junior <juniorjp1989@gmail.com>
2016-10-02 13:39:54 -0300

    insert i18n pt and en flags

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,4 +3,10 @@ class HomeController < ApplicationController
   def index
   end
 
+  def set_user_locale
+    session['locale'] = params[:locale]
+    redirect_to :back
+  rescue ActionController::RedirectBackError
+    redirect_to root_path
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'home#index'
+  get '/home/set_user_locale' => 'home#set_user_locale'
+
   resources :champions
 
   resources :items
